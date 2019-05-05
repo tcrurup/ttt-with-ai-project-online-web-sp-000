18b2d4e91b49757eb35158d48c14693ba87b9a92
Anthony Rurup <tcrurup@gmail.com>
2019-05-05 23:44:58 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -30,15 +30,18 @@ module Players
         #combo match is an array of boolean values that represent a match to the winning
         #combo.  For example if the winning combo is [0,1,2] and my cells are [0,2,5,6]
         #this will be [true, false, true]
+        
         combo_match? = combo.collect{ |x| get_my_cells.include?(x) }
         
         ##If there are two trues in the combo match array that means there is only one more
-        #square needed for a victory
+        #square needed for a victory.  Find the index of the false value and return the 
+        #corresponding winning combination index
+        
         if combo_match?.count{ |x| x == true } == 2
-        ending_cell = combo[]
+          ending_cell = combo[combo_match?.index(false)]
         end  
       end
-    ending_cell
+      ending_cell
     end
     
     def get_cells_with_token(token)
