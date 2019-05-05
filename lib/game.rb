85be6205d9d65657a44d797cfc8e47dea08c2e88
Anthony Rurup <tcrurup@gmail.com>
2019-05-05 23:19:26 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -17,7 +17,6 @@ class Game
     system "clear"
     chosen_move = nil
     self.board.display
-    binding.pry
     until self.board.valid_move?(chosen_move)
       chosen_move = current_player.move
     end
