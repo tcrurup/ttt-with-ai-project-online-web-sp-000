3832e9264c80b516ba5fcb46e4b3e3b672a33d4c
Anthony Rurup <tcrurup@gmail.com>
2019-05-05 22:45:03 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,26 @@
+#Logic tree for computer Players
+#Quip and pause showing the computers turn
+#Check the center square first, take if empty
+#If not empty check for iminent victory
+#If not empty then check for imminent defeat
+#Otherwise take an available corner at random
+#If no corners take a middle spot at random
+
+
+
+
+
 module Players
   class Computer < Player
     
     def move(board = nil)
-      "1"
+      board.cells
+      #if the number of turns is 1 or two, check for the middle square
+      middle_cell_taken?
+    end
+    
+    def middle_cell_taken?(board)
+      
     end
   end
 end
