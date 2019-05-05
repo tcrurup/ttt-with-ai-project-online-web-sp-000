b98964a69ba12cd2a977a9cb1ce59ebcc599c2f2
Anthony Rurup <tcrurup@gmail.com>
2019-05-05 23:42:44 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -23,13 +23,25 @@ module Players
       ["X", "O"].include?(self.board.cells[4])
     end
     
-    def imminent_ending?
+    def imminent_ending?(token)
+      ending_cell = false
       Game::WIN_COMBINATIONS.each do |combo|
-        combo.collect{ |x| get_my_cells.include?(x) }
+        
+        #combo match is an array of boolean values that represent a match to the winning
+        #combo.  For example if the winning combo is [0,1,2] and my cells are [0,2,5,6]
+        #this will be [true, false, true]
+        combo_match? = combo.collect{ |x| get_my_cells.include?(x) }
+        
+        ##If there are two trues in the combo match array that means there is only one more
+        #square needed for a victory
+        if combo_match?.count{ |x| x == true } == 2
+        ending_cell = combo[]
+        end  
       end
+    ending_cell
     end
     
-    def get_my_cells
+    def get_cells_with_token(token)
       my_cells = []
       self.board.each_with_index.each do |value, index|
         my_cells << index if value == self.token
