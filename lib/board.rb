34c4e33eaa72c296dc04eb8c624c6944417c5c42
Anthony Rurup <tcrurup@gmail.com>
2019-05-05 22:14:56 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,54 @@
+class Board
+  
+  attr_accessor :cells
+  
+  def initialize
+    self.reset!
+  end
+  
+  def reset! 
+    @cells = Array.new(9, " ")
+  end
+  
+  def display
+    x = self.cells
+    
+    puts " #{x[0]} | #{x[1]} | #{x[2]} "
+    puts "-----------"
+    puts " #{x[3]} | #{x[4]} | #{x[5]} "
+    puts "-----------"
+    puts " #{x[6]} | #{x[7]} | #{x[8]} "
+  end
+  
+  def position(input)
+    index = input_to_index(input)
+    self.cells[index]
+  end
+  
+  def full?
+    self.cells.collect{ |cell| ["X", "O"].include?(cell) }.all?(true)
+  end
+  
+  def taken?(input)
+    index = input_to_index(input)
+    ["X", "O"].include?(self.cells[index])
+  end
+  
+  def valid_move?(input)
+   input.to_i.between?(1, 9) && !taken?(input) 
+  end
+  
+  def turn_count
+    self.cells.reject{ |cell| cell == " " }.length
+  end
+  
+  def update(input, player)
+    index = input_to_index(input)
+    self.cells[index] = player.token
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+end
---
+++ lib/game.rb
@@ -0,0 +1,90 @@
+class Game
+  
+  attr_accessor :board, :player_1, :player_2
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  def turn 
+    chosen_move = nil
+    until self.board.valid_move?(chosen_move)
+      chosen_move = current_player.move
+    end
+    self.board.update(chosen_move, current_player)
+  end
+  
+  def play 
+    while self.over? == false
+      turn
+    end
+    if self.won? 
+      puts "Congratulations #{self.winner}!"
+    else self.draw?
+      puts "Cat's Game!"
+    end
+  end
+  
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+  
+  def current_player
+    if self.board.turn_count % 2 == 0  
+      self.player_1
+    else
+      self.player_2
+    end
+  end
+  
+  def draw?
+    self.board.full? && !self.won?
+  end
+  
+  def over?
+    self.draw? || self.won?
+  end
+  
+  def winner
+    if self.won? 
+      self.board.cells[won?[0]]
+    else
+      nil
+    end
+  end
+  
+  def won?
+    
+    player_1_cells = []
+    player_2_cells = []
+    
+    self.board.cells.each_with_index do |cell, index|
+      if cell == self.player_1.token 
+        player_1_cells << index
+      elsif cell == self.player_2.token
+        player_2_cells << index
+      end
+    end
+    won = false
+    
+    WIN_COMBINATIONS.each do |win_combo|
+      if win_combo.collect{ |cell| player_1_cells.include?(cell) }.all?(true) || 
+        win_combo.collect{ |cell| player_2_cells.include?(cell) }.all?(true)
+        won = win_combo
+        break
+      end
+    end
+    
+    won
+  end
+
+end
