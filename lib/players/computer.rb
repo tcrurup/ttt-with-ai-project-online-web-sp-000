d46ec30fdf1ec8f1210833973483855dae2afc4a
Anthony Rurup <tcrurup@gmail.com>
2019-05-06 01:44:50 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -47,8 +47,9 @@ module Players
         
         if combo_match.count{ |x| x == true } == 2
           possible_ending_index = combo[combo_match.index(false)]
-          unless self.board.taken?
-          ending_index = possible_ending_index unless self.board.taken?(possible_ending_index) 
+          unless self.board.taken?(possible_ending_index)
+            ending_index = possible_ending_index
+          end
         end  
       end
       
